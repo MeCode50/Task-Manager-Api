baf8a1b32b30a3fb0ee15258e85a62fb57ba0c42
Munachimso <Victorycollins31@gmail.com>
2023-12-10 13:58:03 +0100

    Update: Custom Erroe Handler

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,5 +1,6 @@
 const Task = require("../models/task")
 const asyncWrapper = require("../middleware/async")
+const {createCustomError} = require("../errors/custom-error")
 const getAllTasks = asyncWrapper (async(req,res)=>{
         const task = await Task.find({})
         res.status(200).json({task})
@@ -11,22 +12,23 @@ const createTask = asyncWrapper (async (req,res)=>{
     res.status(201).json({task})   
 })
 
-const getTask  =asyncWrapper (async (req,res)=>{
-        const{id:taskID} = req.params
-        const task = await Task.findOne({_id: taskID});
-        if(!task ){
-            return res.status(404).json({msg:`no task with id :${taskID}`});
-        }
-        res.status(200).json({task})      
-})
+const getTask = asyncWrapper(async (req, res, next) => {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
 
+    if (!task) {
+        return next(createCustomError(`No task with id: ${taskID}`, 404));
+    }
+
+    res.json({ task });
+});
 
 
 const deleteTask = asyncWrapper (async (req,res)=>{
         const {id:taskID} = req.params
         const task = await Task.findOneAndDelete({_id:taskID});
         if(!task ){
-            return res.status(404).json({msg:`no task with id :${taskID}`});
+            return next(createCustomError(`No task with id: ${taskID}`, 404));
         }
         res.status(200).json({ task })
     })
@@ -35,8 +37,11 @@ const deleteTask = asyncWrapper (async (req,res)=>{
             const {id:taskID} = req.params;
                     const task = await Task.findOneAndUpdate({_id: taskID}, req.body,{
                         new:true,
-                        runValidators:true
+                        runValidators:true                
                     })
+                    if(!task ){
+                        return next(createCustomError(`No task with id: ${taskID}`, 404));
+                    }
             res.status(200).json({id:taskID,data:req.body})
     })
 module.exports ={
