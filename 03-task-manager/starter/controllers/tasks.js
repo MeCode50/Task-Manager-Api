a1d40cf122ffcd8cd7e6f02c9043d27379acabdf
Munachimso <Victorycollins31@gmail.com>
2023-12-10 12:02:52 +0100

    Revert "Update: Code Refactoring"
    This reverts commit 3bd81dbc647d85fbdc5edb240f19c6faae8c69d9.

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,29 +1,28 @@
 const Task = require("../models/task")
-const asyncWrapper =  require("../middleware/async")
-
-const getAllTasks = asyncWrapper (async (req,res)=>{ 
+const asyncWrapper = require("../middleware/async")
+const getAllTasks = asyncWrapper (async(req,res)=>{
         const task = await Task.find({})
         res.status(200).json({task})
+    
 })
 
 const createTask = asyncWrapper (async (req,res)=>{
         const task = await Task.create(req.body)
     res.status(201).json({task})   
-    
 })
 
-const getTask  = asyncWrapper (async (req,res)=>{
+const getTask  =asyncWrapper (async (req,res)=>{
         const{id:taskID} = req.params
         const task = await Task.findOne({_id: taskID});
         if(!task ){
             return res.status(404).json({msg:`no task with id :${taskID}`});
         }
-        res.status(200).json({task})
+        res.status(200).json({task})      
 })
 
 
 
-const deleteTask =  asyncWrapper (async (req,res)=>{
+const deleteTask = asyncWrapper (async (req,res)=>{
         const {id:taskID} = req.params
         const task = await Task.findOneAndDelete({_id:taskID});
         if(!task ){
@@ -38,9 +37,8 @@ const deleteTask =  asyncWrapper (async (req,res)=>{
                         new:true,
                         runValidators:true
                     })
-            res.status(200).json({id:taskID,data:req.body})     
+            res.status(200).json({id:taskID,data:req.body})
     })
-    
 module.exports ={
     getAllTasks,
     createTask,
