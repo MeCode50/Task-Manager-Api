c6fac69587200c63429909248405193f3c0d5473
Munachimso <Victorycollins31@gmail.com>
2023-12-10 12:59:27 +0100

    Update: Handled Errors

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -5,16 +5,17 @@ const tasks = require("./routes/tasks")
 const connectDB = require('./db/connect')
 require('dotenv').config()
 const notFound = require("./middleware/not_found")
+const errorHandlerMiddleware = require("./middleware/error-handler")
+
 //middleware
 app.use(express.static("./public"))
 app.use(express.json())
 
-
-
-
 // routes
-app.use('/api/v1/tasks',tasks)
-app.use(notFound)
+app.use('/api/v1/tasks',tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
 
 const port = 3000
 
